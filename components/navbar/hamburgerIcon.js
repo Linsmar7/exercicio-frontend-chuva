268c5d75989a22ada6941598ec6eb640bb2a4832
Linsmar <linsmarvital@gmail.com>
2022-04-16 23:59:42 +0100

    fix hamburger icon zindex

--- components/navbar/hamburgerIcon.js
+++ components/navbar/hamburgerIcon.js
@@ -4,7 +4,7 @@ export default function HamburgerIcon({ onClick }) {
   //HamburgerIcon simples sem precisar baixar mais um svg
   return (
     <div
-      className="space-y-3 fixed top-0 right-0 mt-2 mr-2 lg:hidden"
+      className="space-y-3 fixed top-0 right-0 mt-2 mr-2 lg:hidden z-50"
       onClick={onClick}
     >
       <div className="w-14 h-1.5 bg-orange-300"></div>
